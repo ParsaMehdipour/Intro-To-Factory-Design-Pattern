ed606d129aa9e830ebb32962eb832329a079eb85
Parsa <mehdipourparsa@gmail.com>
2021-06-04 00:30:02 +0430

    feat : add bicycle delivery creator

---
+++ DemoLibrary/BicycleDeliveryCreator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using RealisticDependencies;
+
+namespace DemoLibrary.VehicleTypes
+{
+    public class BicycleDeliveryCreator : DeliveryCreator
+    {
+        public BicycleDeliveryCreator(IAmqpQueue deliveryQueue, IApplicationLogger logger) : base(deliveryQueue, logger)
+        {
+
+        }
+
+        public override IDeliversFood RegisterVehicle()
+        {
+            Bicycle bicycle = new Bicycle()
+            {
+                Color = "blue",
+                Style = "Road",
+                Make = "Trek",
+                Model = "Foo",
+            };
+            _logger.LogInfo("Registering a Bicycle to deliver food!", ConsoleColor.Cyan);
+
+            return bicycle;
+        }
+    }
+}
