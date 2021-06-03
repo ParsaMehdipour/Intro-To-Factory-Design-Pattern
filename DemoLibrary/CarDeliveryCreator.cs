e94336320c1bc6f8a726ffdb474cb8474f7f1f24
Parsa <mehdipourparsa@gmail.com>
2021-06-04 00:30:21 +0430

    feat : add car delivery creator

---
+++ DemoLibrary/CarDeliveryCreator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using DemoLibrary.VehicleTypes;
+using RealisticDependencies;
+
+namespace DemoLibrary
+{
+    public class CarDeliveryCreator : DeliveryCreator
+    {
+        public CarDeliveryCreator(IAmqpQueue deliveryQueue, IApplicationLogger logger) : base(deliveryQueue, logger)
+        {
+
+        }
+
+        public override IDeliversFood RegisterVehicle()
+        {
+            Car car = new Car
+            {
+                Year = 2010,
+                Color = "black",
+                Make = "Honda",
+                Model = "Civic",
+                LicensePlate = "123",
+            };
+            _logger.LogInfo("Registering a Car to deliver food!", ConsoleColor.Green);
+
+            return car;
+        }
+    }
+}
