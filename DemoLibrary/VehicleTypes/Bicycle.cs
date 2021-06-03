a7e4b51d7fcc8ca7a2e6266faf86e365f19f0205
Parsa <mehdipourparsa@gmail.com>
2021-06-04 00:29:21 +0430

    feat : add bicycle class

---
+++ DemoLibrary/VehicleTypes/Bicycle.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+
+namespace DemoLibrary.VehicleTypes
+{
+    public class Bicycle : IDeliversFood
+    {
+        public string Color { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Style { get; set; }
+        public async Task Deliver(int orderId)
+        {
+            // Logic for delivering food
+            await Task.FromResult($"Delivered Order: {orderId} via bicycle!");
+        }
+    }
+}
