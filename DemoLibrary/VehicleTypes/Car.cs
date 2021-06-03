54368513a8519077f858fa5663cbb4062712d93c
Parsa <mehdipourparsa@gmail.com>
2021-06-04 00:29:32 +0430

    feat : add car class

---
+++ DemoLibrary/VehicleTypes/Car.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+
+namespace DemoLibrary.VehicleTypes
+{
+    public class Car : IDeliversFood
+    {
+        public string LicensePlate { get; set; }
+        public string Color { get; set; }
+        public int Year { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+
+        public async Task Deliver(int orderId)
+        {
+            // Logic for delivering food
+            await Task.FromResult($"Delivered Order: {orderId} via car!");
+        }
+    }
+}
